a6e236757b7b0ec5651aa6df8e96b450a0930869
kostya <tsirempilov2014@yandex.ru>
2020-10-19 01:24:25 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:vuueee/SimonsGame.git main:gh-pages
+git push -f git@github.com:vuueee/SimonGame.git master:gh-pages
 
 cd -
